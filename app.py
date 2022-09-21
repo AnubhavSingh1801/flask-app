1f61bb5ffd5e523e33fdbbf441b0dc5fc8247754
Anubhav Singh <anubhav1620.as@gmail.com>
2022-09-22 00:02:40 +0530

    .

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ mysql = MySQL(app)
 
 app.config['MYSQL_DATABASE_USER'] = 'db_user'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'db_password'
-app.config['MYSQL_DATABASE_DB'] = 'employee_test_db'
-app.config['MYSQL_DATABASE_HOST'] = '172.31.12.187'
+app.config['MYSQL_DATABASE_DB'] = 'employee_db'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 
 @app.route('/test')
 def users():
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT * FROM employee_table''')
+    cur.execute('''SELECT user, host FROM mysql.user''')
     rv = cur.fetchall()
     return str(rv)
 
@@ -23,4 +23,4 @@ def main_func():
     return "Hello there!!!"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(debug=True)
