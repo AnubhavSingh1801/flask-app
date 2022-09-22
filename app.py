1bad1a2a9af42b3ba990cae2d7939b83b3f2d6ec
Anubhav Singh <anubhav1620.as@gmail.com>
2022-09-22 12:19:18 +0530

    .

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask
-from flask_mysqldb import MySQL
+# from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-mysql = MySQL(app)
+# mysql = MySQL(app)
 
-app.config['MYSQL_DATABASE_USER'] = 'db_user'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'db_password'
-app.config['MYSQL_DATABASE_DB'] = 'employee_test_db'
-app.config['MYSQL_DATABASE_HOST'] = '172.31.12.187'
-mysql.init_app(app)
+# app.config['MYSQL_DATABASE_USER'] = 'db_user'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'db_password'
+# app.config['MYSQL_DATABASE_DB'] = 'employee_test_db'
+# app.config['MYSQL_DATABASE_HOST'] = '172.31.12.187'
+# mysql.init_app(app)
 
 
-@app.route('/test')
-def users():
-    cur = mysql.connection.cursor()
-    cur.execute('''SELECT * FROM employee_table''')
-    rv = cur.fetchall()
-    return str(rv)
+# @app.route('/test')
+# def users():
+#     cur = mysql.connection.cursor()
+#     cur.execute('''SELECT * FROM employee_table''')
+#     rv = cur.fetchall()
+#     return str(rv)
 
 @app.route('/')
 def main_func():
